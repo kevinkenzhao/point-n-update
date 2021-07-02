d3db330b06ce8ccc16ed67b18bbda9d380b787d8
Kevin Zhao <kevinz8509@gmail.com>
2021-07-02 12:06:27 -0400

    Create dell-update.bat

---
+++ dell-update.bat
@@ -0,0 +1,14 @@
+@echo off
+set /p target="Enter Computer Name (eg. WE7490-JJ5BVU6): "
+
+echo %target% | findstr /r "WE[0-9][0-9][0-9][0-9]-[A-Z0-9]*" >nul 2>&1 && (echo Computer Name OK!) || (echo Invalid Computer Name)
+
+SET "target1=%target:~2%"
+SET "modelnum=%target1:~0,-8%"
+
+echo %modelnum%
+
+xcopy C:\temp\%modelnum%\* \\%target%\c$\temp\%modelnum% /i
+PsExec.exe \\%target% cmd /c "powershell.exe -executionpolicy bypass -windowstyle hidden -noninteractive -nologo -file C:\temp\%modelnum%\Dell.ps1 && DEL C:\temp\%modelnum%\Dell.ps1"
+
+PAUSE
