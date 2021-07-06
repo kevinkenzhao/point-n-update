248da0c45fa4b0d5fd330d302cd98d62a0994ccf
Kevin Zhao <kevinz8509@gmail.com>
2021-07-06 19:45:33 -0400

    Update Dell.ps1

--- Dell.ps1
+++ Dell.ps1
@@ -1,1 +1,7 @@
-Get-ChildItem -Path C:\temp\5060\ -Filter *.exe -Recurse -File -Name | Foreach {Start-Process -FilePath C:\temp\5060\$_ -Argumentlist "/s"; Start-Sleep -Seconds 60}
+param ($modelnum)
+Get-ChildItem -Path C:\temp\$modelnum\ -Filter *.exe -Recurse -File -Name | Foreach {Start-Process -FilePath C:\temp\$modelnum\$_ -Argumentlist "/s"; Start-Sleep -Seconds 60}
+
+<#
+USAGE:
+.\Dell.ps1 -modelnum 5060
+#>
