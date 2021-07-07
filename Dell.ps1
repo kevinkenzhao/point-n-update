840914884b1925105e72c8eec7eb17f04b996813
Kevin Zhao <kevinz8509@gmail.com>
2021-07-06 20:20:18 -0400

    Update Dell.ps1

--- Dell.ps1
+++ Dell.ps1
@@ -1,7 +1,14 @@
-param ($modelnum)
+param([string]$modelnum = "5060") #5060 by default if not parameter is specified
+Write-Host $modelnum
 Get-ChildItem -Path C:\temp\$modelnum\ -Filter *.exe -Recurse -File -Name | Foreach {Start-Process -FilePath C:\temp\$modelnum\$_ -Argumentlist "/s"; Start-Sleep -Seconds 60}
 
 <#
 USAGE:
-.\Dell.ps1 -modelnum 5060
+powershell.exe -ExecutionPolicy Bypass .\Dell.ps1 -modelnum "5070"
+powershell.exe -ExecutionPolicy Bypass .\Dell.ps1 "5070"
+OR
+Set-ExecutionPolicy -ExecutionPolicy Unrestricted -Scope LocalMachine
+.\Dell.ps1 -modelnum "5070"
+Set-ExecutionPolicy -ExecutionPolicy Restricted -Scope LocalMachine
+Get-ExecutionPolicy //Restricted
 #>
